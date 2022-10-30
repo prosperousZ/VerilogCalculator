7ba5ecdb6a4957125f59cafc105ebc589dead5bc
BEVERLY YEE <byee7127@hotmail.com>
2022-10-30 17:25:24 -0600

    Milestone 1 in ReadMe; comments and params in alu.v
    Filling in the Milestone 1 in ReadMe for assignment submission. Added some comments and parameters in alu.v.

--- README.md
+++ README.md
@@ -9,9 +9,16 @@ Contributers:
 A recreation of a simple calculator using Verilog. It serves as a means to both ease back into Verilog and show off the combination of everything learned over the years. And high-level approach to this project is discussed in the Project Proposal file.
 <br/> <br/>
 <b>Milestone 1:</b>
-<ul>
-    <li>Began the implementation of the ALU</li>
-
+<ul style="margin-top:0px">
+    <li>The intention was to get the overall calculator structure down then work on the operations one by one. Successful, but a bit slow moving in that Verilog is rusty for all members. Anticipation is that the process will become faster as the project progresses.<li>
+    <li>Began the implementation of the ALU with a basic structure - have it set up that it will hit case statements and do something. Untested, however.</li>
+    <li>Concerns:
+        <ul style="margin-top:0px">
+            <li>What kind of FPGA does the stockroom have available for us to borrow?</li>
+            <li>How to comminucate with the FPGA? It sounds like there will be some kind of protocol to do so (same with any other potential peripherals).</li>
+            <li>Both the circuit and the FPGA will have their own power source--how will that affect their communcation?</li>
+        </ul>
+    </li>
 </ul>
 <br/>
 <b>Milestone 2:</b>
--- finalProject/alu.v
+++ finalProject/alu.v
@@ -1,46 +1,63 @@
-//This is alu file
 
-
-module alu(a,b,out,mode,neg);
-//implement every operation here like 
 
 //Module to select operation and calculate result
-input [3:0]a,b,mode;
-output [7:0]out;
-reg [7:0] out;
-output reg neg;
-always@(a or b)
-begin
+// Module Name:		alu 
+// Description: 	Module to select operation and caculate the result given 2 inputs.
+// 					All arithmetic calculations will be done in this module.
+//
+// Dependencies: 
+//
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module alu(a,b,out,mode,neg);
+	//implement every operation here like 
 
-//add
-	out = 8'b00000000;
-	if(mode == 4'b0001)
-		begin
-		out = a+b;
-		neg = 0;
-		end
-//sub
-	if(mode == 4'b0100)
-		begin
-		if(b > a)
-			begin 
-			out=b-a;
-			neg = 1;
-			end
-		else
+	// definitions of parameters, inputs, outputs, wires, regs...
+	input [3:0]a,b,mode;
+	output [7:0]out;
+	reg [7:0] out;
+	output reg neg;
+
+	// variable names for modes to make things easier
+	parameter ADD = 4'b0001;
+	parameter SUB = 4'b0010;
+	parameter MUL = 4'b0011;
+	parameter DIV = 4'b0100;
+
+
+	// do something whenever a or b changes
+	always@(a or b)
+	begin
+		//add
+		out = 8'b00000000;
+		if(mode == 4'b0001)
 			begin
-			out=a-b;
+			out = a+b;
 			neg = 0;
 			end
-		end
 
-//mul
-	if(mode == 4'b0010)
-		begin
-		out = a*b;
-		neg =0;
-		end
-//etc.
+		//sub
+		if(mode == 4'b0100)
+			begin
+			if(b > a)
+				begin 
+				out=b-a;
+				neg = 1;
+				end
+			else
+				begin
+				out=a-b;
+				neg = 0;
+				end
+			end
 
-
+		//mul
+		if(mode == 4'b0010)
+			begin
+			out = a*b;
+			neg =0;
+			end
+		//etc.
+	end
 endmodule
