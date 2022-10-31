8dff1ec726fc3f8594ec8cba4e28da5cea5b78c0
BEVERLY YEE <byee7127@hotmail.com>
2022-10-30 18:10:02 -0600

    Added comments, rearranged some things

--- finalProject/finalProject.v
+++ finalProject/finalProject.v
@@ -1,31 +1,41 @@
 
 
-//This would be the main file that run the whole program, basic is a main funciton
+//////////////////////////////////////////////////////////////////////////////////
+// Module Name:		finalProject
+// Description: 	Main file that runs the entire program, like a main function in a C# program.
+//
+// Dependencies: 	clk - clock
+//      					rst - reset
+//					      data - data from the chip interface
+//      					data_clk - clock data from the chip interface
+//					      vga_r - red
+//                vga_g - green
+//                vga_b - blue
+//                vga_clk - clock for the display
+//                vga_blank - ?
+//                hsync - the horizontal sync for vga
+//                vsync - the vertical sync for vga
+//
+// Comments: 		  May need more input/outputs; can be determined as project continues.
+//
+//////////////////////////////////////////////////////////////////////////////////
+module finalProject(clk,rst,data, data_clk, vga_r, vga_g, vga_b, vga_clk, vga_blank, hsync, vsync);
+    input clk, rst, data, data_clk;
 
-//In order to run the program and display on screen, we will need a couple of input, and output
-//Input is the data from alu etc. which is the number 
-//So, vga blue, green, red, is the ouptu, vga clock, horizontal sync, vertical sync, are both output,
-//becuase we want to display on screen
+    output vga_clk, vga_blank, hsync, vsync;
+    output [7:0] vga_r, vga_g, vga_b;
 
-module finalProject(
-
- //clk for this module, rst for this module, data from chip interface, data clk also from chip interface
- //May need more intput, and output
-  input clk,rst,data, data_clk,
-  //color from 0 - 255, for blue, green, and red
-  output [7:0] vga_r, vga_g, vga_b,
+    // hold a 16-bit number--may need to change or add more
+    // will eventually write to IO interface
+    // TODO: change wire to reg (reg is for storage while wires are scalar values)
+    wire[15:0] io_mem, io_write_data, io_mem_address;
   
-  output vga_clk, vga_blank, hsync, vsync
-  );
+    // load the operation from FPGA button, and pass the operation to ALU
+    // TODO: change wire to reg
+    wire io_enable, load_operation, store_operation;
   
-  // 16 bits number, those are probably needed, may need more, wrtie back to io interface 
-  wire [15:0] io_mem, io_write_data, io_mem_address;
-  
-  // load the operation from FPGA button, and pass the opeartion to ALU
-  wire io_enable, load_operation, store_operation;
-  
-  //This is the basic idea how to run, we gonna pass to processor to deal with the data path and ALU etc, 
-  processor proc(clk,rst,io_mem,io_write_data,io_mem_address, io_enable);
+    //This is the basic idea how to run, we gonna pass to processor to deal with the data path and ALU etc, 
+    processor proc(clk,rst,io_mem,io_write_data,io_mem_address, io_enable);
   
   
   //Because we need a lot of variables, to be able to display on screen, may be able to reduce, deal with later
