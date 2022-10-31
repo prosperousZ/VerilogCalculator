0524a61444bc263917c9f078431b1d7f842abeee
BEVERLY YEE <byee7127@hotmail.com>
2022-10-30 19:55:38 -0600

    Added comments, straightened out mux.v

--- finalProject/mux.v
+++ finalProject/mux.v
@@ -1,18 +1,32 @@
-//This is mux file
 
+
+//////////////////////////////////////////////////////////////////////////////////
+// Module Name:		mux 
+// Description: 	   Module to simulate a MUX
+//
+// Dependencies: 	   A -
+//					      B - 
+//					      sel - the operator in which chooses whether A or B is passed through
+//					      Y - output
+//
+// Comments: 		
+//
+//////////////////////////////////////////////////////////////////////////////////
 module mux(A,B,sel,Y);
+   //Our input and output would be 16 bits
+   input [15:0] A, B;
+   input sel;
 
-//Our input and output would be 16 bits
-output [15:0]Y;
-input [15:0] A, B;
-input sel;
-reg[15:0] Y;
+   // these two lines could be combined into output reg [15:0] Y; (?)
+   reg[15:0] Y;
+   output [15:0] Y;
 
-always @(A or B or sel)
-if(sel == 1'b0)
-   Y <= A;
-   else
-   Y <= B;	
-
+   always @(A or B or sel)
+   begin
+      if(sel == 1'b0)
+         Y <= A;
+      else
+         Y <= B;	
+   end
 
 endmodule
