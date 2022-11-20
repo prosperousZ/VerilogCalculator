c8c67db1b903ee9cd3dec7aed9def0db567f968d
BEVERLY YEE <byee7127@hotmail.com>
2022-11-20 13:58:57 -0700

    Update memory.v to Haoze's version

--- finalProject/memory.v
+++ finalProject/memory.v
@@ -8,7 +8,43 @@
 //                  There are templates it can generate.
 //
 //////////////////////////////////////////////////////////////////////////////////
-module memory();
+module memory
+#(parameter DATA_WIDTH=8, parameter ADDR_WIDTH=8)
+(
+	input [(DATA_WIDTH-1):0] address,
+	input clk,
+	input [(ADDR_WIDTH-1):0] data,
+	input wren,
+	output [(DATA_WIDTH-1):0] q
+);
 
+	// Declare the RAM variable
+	reg [DATA_WIDTH-1:0] ram[2**ADDR_WIDTH-1:0];
+
+	// Variable to hold the registered read address
+	reg [ADDR_WIDTH-1:0] addr_reg;
+
+	// Specify the initial contents.  You can also use the $readmemb
+	// system task to initialize the RAM variable from a text file.
+	// See the $readmemb template page for details.
+	initial 
+	begin : INIT
+	    // the folowing path must be modified to reflect the machine being used
+		//$readmemh("/home/haoze/Desktop/ECE6710/FinalProject/Processor/memory.txt",ram);
+	end 
+
+	always @ (posedge clk)
+	begin
+		// Write
+		if (wren)
+			ram[address] <= data;
+
+		addr_reg <= address;
+	end
+
+	// Continuous assignment implies read returns NEW data.
+	// This is the natural behavior of the TriMatrix memory
+	// blocks in Single Port mode.  
+	assign q = ram[addr_reg];
 
 endmodule
