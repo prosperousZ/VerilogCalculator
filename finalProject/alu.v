5d5fef55b25045b76e96768ccf420c7248ed0957
BEVERLY YEE <byee7127@hotmail.com>
2022-11-23 09:15:17 -0700

    Began filling in ALU

--- finalProject/alu.v
+++ finalProject/alu.v
@@ -12,19 +12,19 @@
 // Comments: 		This ALU will operate using the format "a OPERATION b = RESULT"
 //
 //////////////////////////////////////////////////////////////////////////////////
-
-module alu(a, b, mode, out);//, neg);
+//module alu(a, b, out, mode, neg);
+module alu(a, b, mode, out);
 	// definitions of parameters, inputs, outputs, wires, regs...
 	//Let me take neg first to test
 	
 	input [7:0] a, b;
 	
-	//Since the FPGA only have 10 switch, I think 8 of them for input, only two of them for operation
+	// FPGA has only 10 switches: 8 will be numbers, 2 will be operations
+	// which means some numbers and operations will be a combination of 2 switches.
 	input [1:0] mode;
 
-
 	output reg [7:0] out;
-	//output reg neg;
+	output reg neg;
 
 	// variable names for modes to make things easier
 	parameter ADD = 2'b00;
@@ -32,44 +32,65 @@ module alu(a, b, mode, out);//, neg);
 	parameter MUL = 2'b10;
 	parameter DIV = 2'b11;
 
+	// any wires needed
+	wire negA;
+	wire negB;
+	wire tempA;
+	wire tempB;
+
 	// do something whenever a or b changes -> may need more on the list
 	always@(a or b)
 	begin
 		out = 8'b00000000;
 
+		// check to see if either operand is negative
+		tempA = a >> 8;		// right shift 8 (or is it 7?) spaces to get leading 1
+		tempB = b >> 8;
+		if(tempA == 1)
+			negA = 1;
+		else
+			negA = 0;
+		if (tempB == 1)
+			negB = 1;
+		else
+			negB = 0;
+
 		case(mode)
-			ADD:
+			ADD:	// 00
 			begin
+				
 				out = a + b;
-				//neg = 0;
+				neg = 0;
 			end
 
-			SUB:
+			SUB:	// 01
 			begin
 				if(b > a)
-					begin
-						out = b - a;
-						//neg = 1;
-					end
+				begin
+					// a = 1st operand, b = 2nd operand
+					// so if b is greater, have it become the first operand and set neg = 1
+					out = b - a;
+					neg = 1;
+				end
 				else
-					begin
-						out = a - b;
-						//neg = 0;
-					end
+				begin
+					// otherwise normal operation
+					out = a - b;
+					neg = 0;
+				end
 			end
 
-			MUL:
+			MUL:	// 10
 			begin
 				out = a * b;
 				// neg = 0 only if both a and b are positive or negative
 				//neg = 0;
 			end
 
-			DIV:
+			DIV:	// 11
 			begin
 				// do nothing for now
 			end
-	endcase
-	
+		endcase
 	end
 endmodule
