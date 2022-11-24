74183e6d7a5c83d3e1bf1a03817b1ee76c192152
Haoze Zhang <zhz1031108486@gmail.com>
2022-11-24 02:50:41 -0700

    Update finalProject.v

--- finalProject/finalProject.v
+++ finalProject/finalProject.v
@@ -1,3 +1,4 @@
+
 //////////////////////////////////////////////////////////////////////////////////
 // Module Name:		finalProject
 // Description: 	Main file that runs the entire program, like a main function in a C# program.
@@ -10,35 +11,37 @@
 //                vga_g - green
 //                vga_b - blue
 //                vga_clk - clock for the display
-//                vga_blank - ?
+//                vga_blank - blank for display, for display nothing on the screen.
 //                hsync - the horizontal sync for vga
 //                vsync - the vertical sync for vga
 //
 // Comments: 		  May need more input/outputs; can be determined as project continues.
 //
 //////////////////////////////////////////////////////////////////////////////////
-module finalProject(clk,rst,data, data_clk, vga_r, vga_g, vga_b, vga_clk, vga_blank, hsync, vsync);
-    input clk, rst, data, data_clk;
-
-    output vga_clk, vga_blank, hsync, vsync;
-    output [7:0] vga_r, vga_g, vga_b;
-
-    // hold a 8-bit number--may need to change or add more
-    // will eventually write to IO interface
-    // TODO: change wire to reg (reg is for storage while wires are scalar values)
-    wire[7:0] io_mem, io_write_data, io_mem_address;
+module finalProject(
+	input clk,rst,ps2_clk,ps2_data,
+	output [7:0] vga_r, vga_g, vga_b,
+	output vga_clk, vga_blank_n, hsync,vsync
+	); 
+	wire [7:0] iomem_q,io_write_data,iomem_addr;
+	wire IO_w_en, load_op, stor_op;
+	processor proc(clk, rst, iomem_q, io_write_data, iomem_addr, IO_w_en);
+	
+	//io_interface ioint(clk, rst, IO_w_en,load_op,stor_op,ps2_clk,ps2_data,
+		//iomem_addr,io_write_data,0,0,
+		//iomem_q,
+		//vga_clk,vga_blank_n, hsync, vsync,op_complete,vga_r,vga_g,vga_b);
   
-    // load the operation from FPGA button, and pass the operation to ALU
-    // TODO: change wire to reg
-    wire io_enable, load_operation, store_operation;
-  
-    //This is the basic idea how to run, we gonna pass to processor to deal with the data path and ALU etc, 
-    processor proc(clk, rst, io_mem, io_write_data, io_mem_address, io_enable);
+  //Because we need a lot of variables, to be able to display on screen, may be able to reduce, deal with later
+  //just write for concept.
   
   
   //*****************concept structure of io**************************
-  // may not need as we are not really interfacing with another chip at this time
+  
   //io_interface io_int(clk, rst, io_enable,load_operation, store_operation, data_clk, data,
   //                  io_mem_address, io_write_data, io_mem,
-  //					 vga_clk, vga_blank, hsync, vsync, vga_r, vga_g, vga_b); 
+  //					 vga_clk, vga_blank, hsync, vsync, vga_r, vga_g, vga_b);
+							 
+  //*****************can be uncommen****************************************
+ 
 endmodule
