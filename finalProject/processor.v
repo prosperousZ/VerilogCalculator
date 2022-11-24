e1953ce290ecdbed3a25a2f683f96af8fd746827
Haoze Zhang <zhz1031108486@gmail.com>
2022-11-24 02:54:08 -0700

    Update processor.v

--- finalProject/processor.v
+++ finalProject/processor.v
@@ -7,12 +7,12 @@
 module processor(clk, reset, Q, data, addr, io_enable);
 
 input clk, reset;
-input [15:0] Q;
+input [7:0] Q;
 
 //My add additional variables here
 
 
-output[15:0] data, addr;
+output[7:0] data, addr;
 output io_enable;
 
 //pass to datapah, may do something else in processor file
