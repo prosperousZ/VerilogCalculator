184cdace2740365b3fd910e070a507ef8baaaba3
Haoze Zhang <zhz1031108486@gmail.com>
2022-11-24 02:54:56 -0700

    Update tb_alu.v

--- finalProject/tb_alu.v
+++ finalProject/tb_alu.v
@@ -1,40 +1,45 @@
-
-
-//////////////////////////////////////////////////////////////////////////////////
-// Module Name:		tb_alu 
-// Description: 	Testbench file for the alu
-//
-// Dependencies: 	
-//
-// Comments: 		Test PASS
-//
-//////////////////////////////////////////////////////////////////////////////////
+//This is tb_alu file
 `timescale 1ns / 1ps
+
 module tb_alu;
 
-    parameter WIDTH = 8;
-    reg [WIDTH -1:0] A,B;
-    reg [1:0]  mode;
-    wire [WIDTH -1: 0] out;
+//Test pass
 
-    alu AA(.a(A), .b(B), .mode(mode), .out(out));
+parameter WIDTH = 8;
+reg [WIDTH -1:0] A,B;
+reg [1:0]  mode;
+wire [WIDTH -1: 0] out;
 
-    initial begin
-        //test add
-        mode = 2'b00;
-        
-        A = 8;
-        B = 9;
-    
-        #15;
-        $display("result is :%d", out);
+alu AA(.a(A), .b(B), .mode(mode), .out(out));
 
-        // test 2
-        A = 80;
-        B = 90;
+initial begin
 
-        #15;
-        $display("result is :%d", out);
-    end
+
+
+//test add
+mode = 2'b00;
+A = 8;
+B = 9;
+
+
+#15;
+$display("result is :%d", out);
+
+
+
+
+A = 80;
+B = 90;
+
+
+#15;
+$display("result is :%d", out);
+
+
+
+
+end
+
+
 
 endmodule
