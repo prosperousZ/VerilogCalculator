8a524e1137cfb6cfaf12c3ab36299412275df5ae
Ekata Mitra <83133600+EkataU@users.noreply.github.com>
2022-11-26 09:11:37 -0700

    negative values updated

--- finalProject/alu.v
+++ finalProject/alu.v
@@ -12,8 +12,8 @@
 // Comments: 		This ALU will operate using the format "a OPERATION b = RESULT"
 //
 //////////////////////////////////////////////////////////////////////////////////
-//module alu(a, b, out, mode, neg);
-module alu(a, b, mode, out);
+module alu(a, b, out, mode, neg);
+//module alu(a, b, mode, out);
 	// definitions of parameters, inputs, outputs, wires, regs...
 	//Let me take neg first to test
 	
@@ -32,18 +32,22 @@ module alu(a, b, mode, out);
 	parameter MUL = 2'b10;
 	parameter DIV = 2'b11;
 
-	// any wires needed
+	wire [7:0] tempA;
+	wire [7:0] temp;
+
+	
+/*	// any wires needed
 	wire negA;
 	wire negB;
 	wire tempA;
 	wire tempB;
-
+*/
 	// do something whenever a or b changes -> may need more on the list
-	always@(a or b)
+	always@(a or b or mode)
 	begin
 		out = 8'b00000000;
 
-		// check to see if either operand is negative
+/*		// check to see if either operand is negative
 		tempA = a >> 8;		// right shift 8 (or is it 7?) spaces to get leading 1
 		tempB = b >> 8;
 		if(tempA == 1)
@@ -54,7 +58,7 @@ module alu(a, b, mode, out);
 			negB = 1;
 		else
 			negB = 0;
-
+*/
 		case(mode)
 			ADD:	// 00
 			begin
@@ -66,6 +70,7 @@ module alu(a, b, mode, out);
 			SUB:	// 01
 			begin
 				if(b > a)
+
 				begin
 					// a = 1st operand, b = 2nd operand
 					// so if b is greater, have it become the first operand and set neg = 1
@@ -78,6 +83,7 @@ module alu(a, b, mode, out);
 					out = a - b;
 					neg = 0;
 				end
+
 			end
 
 			MUL:	// 10
