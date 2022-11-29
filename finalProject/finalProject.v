e6db44c2652e77b77040971966036ca32c112b57
Haoze Zhang <zhz1031108486@gmail.com>
2022-11-29 16:29:01 -0700

    Update finalProject.v

--- finalProject/finalProject.v
+++ finalProject/finalProject.v
@@ -1,4 +1,3 @@
-
 //////////////////////////////////////////////////////////////////////////////////
 // Module Name:		finalProject
 // Description: 	Main file that runs the entire program, like a main function in a C# program.
@@ -12,15 +11,16 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 
+//Author:Haoze Zhang
+
 module finalProject (clk, reset, r0, r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15);
 
 input clk, reset;
 wire regwrite;
 wire [3:0] wa, immLo, ra1, ra2;
-wire [7:0] aluop, ld_pc_disp;
-wire  we_a, LD_mux_en_a, pc_en, ld_pc_en,pc_mux, flag_en, wr_pc;
+wire [7:0] aluop;
+wire  we_a, LD_mux_en_a, pc_en, ld_pc_en,pc_mux,wr_pc;
 wire [15:0] q_a; 
-wire [4:0] flags_out;
 output [15:0] r0, r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15;
 
 dataPath DP(reset, clk, r0, r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15);
