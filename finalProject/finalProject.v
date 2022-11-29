5079a110b29101f9ef51a61fc03a2c8ea63f4498
Haoze Zhang <zhz1031108486@gmail.com>
2022-11-29 16:14:48 -0700

    Update finalProject.v

--- finalProject/finalProject.v
+++ finalProject/finalProject.v
@@ -7,41 +7,23 @@
 //      					rst - reset
 //					      data - data from the chip interface
 //      					data_clk - clock data from the chip interface
-//					      vga_r - red
-//                vga_g - green
-//                vga_b - blue
-//                vga_clk - clock for the display
-//                vga_blank - blank for display, for display nothing on the screen.
-//                hsync - the horizontal sync for vga
-//                vsync - the vertical sync for vga
 //
 // Comments: 		  May need more input/outputs; can be determined as project continues.
 //
 //////////////////////////////////////////////////////////////////////////////////
-module finalProject(
-	input clk,rst,ps2_clk,ps2_data,
-	output [7:0] vga_r, vga_g, vga_b,
-	output vga_clk, vga_blank_n, hsync,vsync
-	); 
-	wire [7:0] iomem_q,io_write_data,iomem_addr;
-	wire IO_w_en, load_op, stor_op;
-	processor proc(clk, rst, iomem_q, io_write_data, iomem_addr, IO_w_en);
-	
-	//io_interface ioint(clk, rst, IO_w_en,load_op,stor_op,ps2_clk,ps2_data,
-		//iomem_addr,io_write_data,0,0,
-		//iomem_q,
-		//vga_clk,vga_blank_n, hsync, vsync,op_complete,vga_r,vga_g,vga_b);
-  
-  //Because we need a lot of variables, to be able to display on screen, may be able to reduce, deal with later
-  //just write for concept.
-  
-  
-  //*****************concept structure of io**************************
-  
-  //io_interface io_int(clk, rst, io_enable,load_operation, store_operation, data_clk, data,
-  //                  io_mem_address, io_write_data, io_mem,
-  //					 vga_clk, vga_blank, hsync, vsync, vga_r, vga_g, vga_b);
-							 
-  //*****************can be uncommen****************************************
- 
-endmodule
+
+module finalProject (clk, reset, r0, r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15);
+
+input clk, reset;
+wire regwrite;
+wire [3:0] wa, immLo, ra1, ra2;
+wire [7:0] aluop, ld_pc_disp;
+wire  we_a, LD_mux_en_a, pc_en, ld_pc_en,pc_mux, flag_en, wr_pc;
+wire [15:0] q_a; 
+wire [4:0] flags_out;
+output [15:0] r0, r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15;
+
+dataPath DP(reset, clk, r0, r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15);
+
+
+endmodule 
