cb195a96cf541ae676692c4674b811ab8cfe3f67
Haoze Zhang <zhz1031108486@gmail.com>
2022-11-29 16:19:34 -0700

    Update pc.v

--- finalProject/pc.v
+++ finalProject/pc.v
@@ -9,14 +9,14 @@
 
 module pc(input clk,
     input reset,
-	 input [7:0] ld_pc,
+	 input [15:0] ld_pc,
 	 input wr_pc,
-    output reg [7:0] pc);
+    output reg [15:0] pc);
 	 
 	 
 	initial begin
 	
-		pc <= 8'h00;
+		pc <= 16'h0000;
 	end
 
 	
@@ -25,7 +25,7 @@ module pc(input clk,
     begin
     	if (reset == 1)
     	begin
-    		pc <= 8'h00;
+    		pc <= 16'h0000;
     	end
     	else
     	begin
